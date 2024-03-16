3a4ad2b52487c879d6daf49ef7373e829bed35e7
Serge Smertin <serge.smertin@databricks.com>
2024-03-16 15:28:19 +0100

    (WIP) Experimental notebook cell magic `%%pylint`
    Related to #20

---
+++ scripts/runner.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+
+# COMMAND ----------
+
+# MAGIC %pip install ./databricks_labs_pylint-0.0.0-py3-none-any.whl --force-reinstall
+dbutils.library.restartPython()  # noqa
+# MAGIC %load_ext databricks.labs.pylint.magic
+
+# COMMAND ----------
+
+from databricks.labs.pylint.magic import enable
+enable()
+
+# COMMAND ----------
+
+# MAGIC %%pylint
+# MAGIC print('dbfs:foo')
